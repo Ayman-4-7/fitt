2a02f6708c5b0124001f083432f86ffc1c280499
Ayman Muhamed <ayman2017147@gmail.com>
2025-04-29 23:55:22 +0000

    asf

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Login from "./pages/Login"
-import Exercises from "./pages/Exercises"
-import QandA from "./pages/QandA"
-import HomePlan from "./pages/HomePlan"
-import GymPlan from "./pages/GymPlan"
-import DashboardLayout from "./components/DashboardLayout"
-import EditG from "./pages/EditG"
-import EditMemberShip from "./pages/EditMemberShip"
-import EditExercise from "./pages/EditExercise"
-import Dashboard from "./pages/Dashboard"
-import Supplements from "./pages/Supplements"
-import Membership from "./pages/Membership"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Exercises from "./pages/Exercises";
+import QandA from "./pages/QandA";
+import HomePlan from "./pages/HomePlan";
+import GymPlan from "./pages/GymPlan";
+import DashboardLayout from "./components/DashboardLayout";
+import EditG from "./pages/EditG";
+import EditMemberShip from "./pages/EditMemberShip";
+import EditExercise from "./pages/EditExercise";
+import Dashboard from "./pages/Dashboard";
+import Supplements from "./pages/Supplements";
+import Membership from "./pages/Membership";
 
-
+aymannnnnnnnnnnnnn;
 function App() {
   return (
     <Router>
@@ -40,17 +40,12 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/supplements" element={<Supplements />} />
             <Route path="/membership" element={<Membership />} />
-
-
-
-          
           </Routes>
         </main>
         <Footer />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
-
+export default App;
