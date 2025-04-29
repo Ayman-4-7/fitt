3a82da5e35713a684dcea4f6eb6d54186ca85860
Ayman Muhamed <ayman2017147@gmail.com>
2025-04-29 23:33:03 +0000

    f

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
-
+  server: {
+    allowedHosts: ["5lv6hf-5173.csb.app"], // Add this line
+  },
+});
